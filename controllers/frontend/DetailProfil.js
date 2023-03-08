04cda4dd0511ab125337a3da9b1f68d5f2f357e0
Ristan Alfatha Aditara <ristankrenz@gmail.com>
2023-03-08 13:04:55 +0800

    detail profil

--- controllers/frontend/DetailProfil.js
+++ controllers/frontend/DetailProfil.js
@@ -1,6 +1,10 @@
+import Article from "../../models/articleModel.js";
+import City from "../../models/cityModel.js";
 import Destination from "../../models/destinationModel.js"
 import GalleryDetail from "../../models/galleryDetailModel.js";
 import Gallery from "../../models/galleryModel.js";
+import Officer from "../../models/officerModel.js";
+import Province from "../../models/provinceModel.js";
 
 export const getDetailLocation = async (req, res) => {
     try {
@@ -8,7 +12,18 @@ export const getDetailLocation = async (req, res) => {
             where: {
                 id: req.params.id
             },
-            attributes: ['id', 'destination', 'address', 'url'],
+            attributes: ['id', 'kdProv', 'destination', 'address', 'url', 'description','embMap'],
+        })
+
+        const province = await Province.findOne({
+            where: {
+                id: data.toJSON().kdProv
+            },
+        })
+        const city = await City.findOne({
+            where: {
+                id: province.toJSON().id
+            },
         })
 
         const galleryName = await Gallery.findAll({
@@ -22,17 +37,42 @@ export const getDetailLocation = async (req, res) => {
             const image = await GalleryDetail.findAll({
                 where: {
                     galleryId: id
-                }
+                },
             })
             return image
         }
-        const gallery = await Promise.all(
-            galleryName.map((gallery) => {
-                return getGalleryImage(gallery.id)
+
+        const galleries = await Promise.all(
+            galleryName.map(async (gallery) => {
+                const images = await getGalleryImage(gallery.id);
+                return {
+                    title: gallery.title,
+                    images: images,
+                };
             })
         )
 
-        return res.json({ ...data.toJSON(), image: gallery });
+        const pengelola = await Officer.findAll({
+            where: {
+                destinationId: data.toJSON().id
+            },
+            attributes: ['id', 'nameOfficer']
+        })
+        const article = await Article.findAll({
+            where: {
+                destinationId: data.toJSON().id
+            },
+            attributes: ['id', 'title', 'url']
+        })
+
+        return res.json({
+            ...data.toJSON(),
+            province: province.toJSON().province,
+            city: city.toJSON().city,
+            gallery: galleries,
+            pengelola: pengelola,
+            article: article,
+        });
     } catch (error) {
         return res.json(error.message);
     }
--- models/provinceModel.js
+++ models/provinceModel.js
@@ -1,14 +1,15 @@
 import { Sequelize } from "sequelize";
 import db from "../config/database.js";
+import Destination from "./destinationModel.js";
 
 const { DataTypes } = Sequelize;
- 
-const Province = db.define('t_provinces',{
-    province:{
+
+const Province = db.define('t_provinces', {
+    province: {
         type: DataTypes.STRING
     },
-},{
+}, {
     freezeTableName: true
 });
- 
+
 export default Province;
