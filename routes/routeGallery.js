950d314c602fab30d373eccf187a9a8575a60aa8
mfahmifadhs <mfahmifadh2@gmail.com>
2023-03-10 08:26:36 +0700

    backend

--- routes/routeGallery.js
+++ routes/routeGallery.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { createGallery, deleteGallery, getGallery, getGalleryById, updateGallery } from "../controllers/Gallery.js";
+import { deleteDetailGallery, getDetailGallery, getDetailGalleryById } from "../controllers/GalleryDetail.js";
 
 const routeGallery = express.Router();
 
@@ -8,5 +9,10 @@ routeGallery.post('/gl', createGallery);
 routeGallery.get('/gl/detail/:id', getGalleryById);
 routeGallery.patch('/gl/:id', updateGallery);
 routeGallery.delete('/gl/:id', deleteGallery);
+
+routeGallery.get('/gd/:id', getDetailGallery);
+routeGallery.get('/gd/detail/:id', getDetailGalleryById);
+routeGallery.patch('/gd/update/:galleryId/:id', updateGallery);
+routeGallery.delete('/gd/:id', deleteDetailGallery);
 
 export default routeGallery;
