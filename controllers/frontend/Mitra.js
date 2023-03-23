aeb3b6c94e2b3c3fe46b4eefe216748c042ec9d4
Ristan Alfatha Aditara <ristankrenz@gmail.com>
2023-03-23 21:03:12 +0800

    add hyperlink in mitra

--- controllers/frontend/Mitra.js
+++ controllers/frontend/Mitra.js
@@ -10,32 +10,32 @@ export const getDescription = async (req, res) => {
                 menu: 'Mitra'
             }
         })
-        result.push({title: "Kementrian / Lembaga", desc: description.toJSON().description})
-        
+        result.push({ title: "Kementrian / Lembaga", desc: description.toJSON().description })
+
         const pemda = await Partner.findAll({
-            attributes: ['id', 'partner', 'url'],
+            attributes: ['id', 'partner', 'url', 'hyperlink'],
             where: {
                 category: 'Pemerintah Daerah'
             }
         })
-        result.push({title: "Pemerintah Daerah", mitra: pemda})
+        result.push({ title: "Pemerintah Daerah", mitra: pemda })
 
         const NGO = await Partner.findAll({
-            attributes: ['id', 'partner', 'url'],
+            attributes: ['id', 'partner', 'url', 'hyperlink'],
             where: {
                 category: 'Non-Government Organization'
             }
         })
-        result.push({title: "Non-Government Organization", mitra: NGO})
+        result.push({ title: "Non-Government Organization", mitra: NGO })
 
         const PS = await Partner.findAll({
-            attributes: ['id', 'partner', 'url'],
+            attributes: ['id', 'partner', 'url', 'hyperlink'],
             where: {
                 category: 'Private Sector'
             }
         })
-        result.push({title: "Private Sector", mitra: PS})
-        
+        result.push({ title: "Private Sector", mitra: PS })
+
         return res.json(result);
     } catch (error) {
         return res.json(error.message);
